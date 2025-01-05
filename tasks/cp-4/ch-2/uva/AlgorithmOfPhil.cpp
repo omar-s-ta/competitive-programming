acbff9350bfad239a6c404ca63a3e3bdefd3f434
Omar Abdelrahman <omar.sebres@gmail.com>
2025-01-05 21:55:33 +0100

    AlgorithmOfPhil

---
+++ tasks/cp-4/ch-2/uva/AlgorithmOfPhil.cpp
@@ -0,0 +1,51 @@
+#include "../../../../library/io.hpp"
+
+using namespace io;
+
+class AlgorithmOfPhil {
+public:
+  inline void mod(int& a, const int b) {
+    if (a >= b) a -= b;
+  }
+
+  int solveOne(const std::string& a) {
+    const int n = a.size();
+    int result = 0;
+    int R = n / 2;
+    int L = R - 1;
+    if (n % 2 != 0) {
+      result += (a[R] - '0');
+      R += 1;
+    }
+    for (; L >= 0; L--, R++) {
+      result <<= 1;
+      mod(result, MOD);
+      result <<= 1;
+      mod(result, MOD);
+      const int s = (a[L] - '0') + (a[R] - '0');
+      if (s == 0) continue;
+      result += (s + 1);
+      mod(result, MOD);
+    }
+    return result;
+  }
+
+  void solve() {
+    int nt;
+    in >> nt;
+    for (int _ = 1; _ <= nt; _++) {
+      std::string a;
+      in >> a;
+      out << "Case #" << _ << ": " << solveOne(a) << '\n';
+    }
+  }
+
+  AlgorithmOfPhil(std::istream& in, std::ostream& out)
+    : in(in), out(out) { }
+
+private:
+  std::istream& in;
+  std::ostream& out;
+
+  const int MOD = (1e9) + 7;
+};
