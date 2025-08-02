1f38ec76a248b5a19177bd7dbb23fc1cd0733e43
Omar Abdelrahman <omar.sebres@gmail.com>
2025-08-02 12:12:15 +0200

    SnapperChainHard explanation

--- tasks/cp-4/ch-2/kattis/SnapperChainHard.cpp
+++ tasks/cp-4/ch-2/kattis/SnapperChainHard.cpp
@@ -2,6 +2,27 @@
 
 using namespace io;
 
+/**
+ * Simulate the snapping process:
+ * n = 4
+ * initial: 0000
+ * snap 1 : 1000
+ * snap 2 : 0100
+ * snap 3 : 1100
+ * snap 4 : 0010
+ * snap 5 : 1010
+ * snap 6 : 0110
+ * snap 7 : 1110
+ * snap 8 : 0001
+ * ...
+ *
+ * Eventually you can see that it will be 'ON' only when all snappers are on which
+ * will have this shape for every number n -> (2^n)-1 = (1<<n)-1
+ *
+ * So if after K snaps it reaches the above shape it should be 'ON', otherwise 'OFF'.
+ *
+ * The only special case is when n=1. You can figure this out easily.
+ */
 class SnapperChainHard {
 public:
   void solveOne(const int ci, int n, int k) {
