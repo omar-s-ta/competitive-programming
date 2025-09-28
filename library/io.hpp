1b0485248f606c00fd95d370b92fe42d8be75110
Omar Saber Talaat Abdelrahman <omar.abdelrahman.de@gmail.com>
2025-09-29 00:36:32 +0200

    'std::println' is enough for now

--- library/io.hpp
+++ library/io.hpp
@@ -5,26 +5,10 @@
 #include <vector>
 #include <deque>
 #include <array>
-#include <cstdint>
 #include <cstdio>
-#include <string>
-#include <algorithm>
-#include <unordered_map>
-#include <sstream>
 #include <utility>
-#include <numeric>
-#include <chrono>
-#include <random>
-#include <iomanip>
 #include <cassert>
 #include <cstring>
-#include <bitset>
-#include <bit>
-#include <set>
-#include <memory>
-#include <type_traits>
-#include <queue>
-#include <stack>
 
 namespace io {
 
@@ -83,61 +67,6 @@ namespace io {
     (in >> ... >> ts);
   }
 #endif
-
-  template<typename T>
-  inline void std_print(std::ostream& out, T&& t) { out << t; }
-  template<typename H, typename... T>
-  inline void std_print(std::ostream& out, H&& h, T&&... t) {
-    std_print(out, h);
-    out << '\n';
-    std_print(out, t...);
-    if (sizeof...(t) == 1) out << '\n';
-  }
-
-  inline void delim_print(std::ostream& out, const char*, const char c)         { out << c; }
-  inline void delim_print(std::ostream& out, const char*, const int8_t i)       { out << i; }
-  inline void delim_print(std::ostream& out, const char*, const uint8_t i)      { out << i; }
-  inline void delim_print(std::ostream& out, const char*, const int16_t i)      { out << i; }
-  inline void delim_print(std::ostream& out, const char*, const uint16_t i)     { out << i; }
-  inline void delim_print(std::ostream& out, const char*, const int i)          { out << i; }
-  inline void delim_print(std::ostream& out, const char*, const uint32_t i)     { out << i; }
-  inline void delim_print(std::ostream& out, const char*, const int64_t i)      { out << i; }
-  inline void delim_print(std::ostream& out, const char*, const uint64_t i)     { out << i; }
-  inline void delim_print(std::ostream& out, const char*, const double d)       { out << d; }
-  inline void delim_print(std::ostream& out, const char*, const long double d)  { out << d; }
-  inline void delim_print(std::ostream& out, const char*, const float f)        { out << f; }
-  inline void delim_print(std::ostream& out, const char*, const std::string& s) { out << s; }
-  template<typename T>
-  inline void delim_print(std::ostream& out, const char*, T&& t) { out << t; }
-  template<typename U, typename V>
-  inline void delim_print(std::ostream& out, const char* delim, const std::pair<U, V>& p) {
-    delim_print(out, delim, p.first);
-    out << delim;
-    delim_print(out, delim, p.second);
-  }
-  template<typename T>
-  inline void delim_print(std::ostream& out, const char* delim, const std::vector<T>& ts) {
-    const std::size_t n = ts.size();
-    for (std::size_t i = 0; i < n; i++) {
-      if (i > 0) out << delim;
-      delim_print(out, delim, ts[i]);
-    }
-  }
-  template<typename T>
-  inline void delim_print(std::ostream& out, const char* delim, const std::deque<T>& ts) {
-    const std::size_t n = ts.size();
-    for (std::size_t i = 0; i < n; i++) {
-      if (i > 0) out << delim;
-      delim_print(out, delim, ts[i]);
-    }
-  }
-  template<typename H, typename... T>
-  inline void delim_print(std::ostream& out, const char* delim, H&& h, T&&... t) {
-    delim_print(out, delim, h);
-    out << delim;
-    delim_print(out, delim, t...);
-    if (sizeof...(t) == 1) out << '\n';
-  }
 }
 
 #endif
